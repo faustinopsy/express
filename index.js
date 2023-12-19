4ac208349366fd360f8c998d89bfff55e89c1e6a
faustinopsy <rodrigohipnose@gmail.com>
2023-12-19 19:47:35 -0300

    criando um modulo da sorte

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { engine } = require('express-handlebars');
-
+const sorteDoDia = require('./lib/sorte')
 const app = express();
 
 app.engine('handlebars', engine({
@@ -14,8 +14,7 @@ app.use(express.static(__dirname + '/public'))
 
 // app.get('/',(req , res) => res.render('home'))
 app.get('/',(req , res) => {
-    const sorteDoDia = sorte[Math.floor(Math.random()*sorte.length)]
-    res.render('home', {sorte: sorteDoDia})
+    res.render('home', {sorte: sorteDoDia.getSorteDoDia()})
 })
 
 app.get('/sobre',(req , res) => res.render('sobre'))
@@ -30,12 +29,7 @@ app.use((req , res) => {
     res.render('500')
 })
 
-const sorte=[
-    'você ganhou 10 milhões na mega sena',
-    'você ganhou 1 milhão e meio na lotofácil',
-    'você dividiu o premio principal da lotofácil com 1 pessoa',
-    'você dividiu o premio principal da mega sena com 3 pessoas'
-]
+
 
 app.listen(port, () => console.log(`Express inicado em http://localhost:${port}` 
 + ' Precione Ctrl+c para encerrar'))
