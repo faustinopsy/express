8e53d342e78161b76522a3884c54c1c33ea7ed53
faustinopsy <rodrigohipnose@gmail.com>
2023-12-19 13:28:24 -0300

    Criando uma rota simples

--- hello.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) =>{
-    res.writeHead(200,{ 'content-Type':'text/html'})
-    res.end('ola mundo!')
-})
-
-server.listen(port, ()=> console.log(`servidor iniciado na porta ${port};` + 'precione Ctrl+C para terminar...'))
