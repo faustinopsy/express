deb0a480ee555ab40aae91e15c44cdc5e80b5f03
faustinopsy <rodrigohipnose@gmail.com>
2023-12-20 18:03:57 -0300

    modularizando as rotas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { engine } = require('express-handlebars');
-const sorteDoDia = require('./lib/sorte')
+const rotas = require('./lib/rotas')
 const app = express();
 
 app.engine('handlebars', engine({
@@ -12,22 +12,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/',(req , res) => res.render('home'))
-app.get('/',(req , res) => {
-    res.render('home', {sorte: sorteDoDia.getSorteDoDia()})
-})
+app.get('/',rotas.home)
 
-app.get('/sobre',(req , res) => res.render('sobre'))
+app.get('/sobre',rotas.sobre)
 
-app.use((req , res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(rotas.notfound)
 
-app.use((req , res) => {
-    res.status(500)
-    res.render('500')
-})
+app.use(rotas.serverError)
 
 
 
