8151a2372200a8cf84c13db7a4050a792cb5f676
faustinopsy <rodrigohipnose@gmail.com>
2023-12-20 18:25:14 -0300

    modified:   __test__/rotas.test.js

--- __test__/rotas.test.js
+++ __test__/rotas.test.js
@@ -4,17 +4,30 @@ test('página inicial', () =>{
     const req = {}
     const res = { render: jest.fn() }
     rotas.home(req, res)
+    expect(res.render.mock.lenght).toBe(undefined)
     expect(res.render.mock.calls[0][0]).toBe('home')
 })
 test('página sobre', () =>{
     const req = {}
     const res = { render: jest.fn() }
     rotas.sobre(req, res)
+    expect(res.render.mock.lenght).toBe(undefined)
     expect(res.render.mock.calls[0][0]).toBe('sobre')
 })
 test('página 404', () =>{
     const req = {}
     const res = { render: jest.fn() }
     rotas.notfound(req, res)
+    expect(res.render.mock.lenght).toBe(undefined)
     expect(res.render.mock.calls[0][0]).toBe('404')
+})
+test('página 500', () =>{
+    const err = new Error('erro interno')
+    const req = {}
+    const res = { render: jest.fn() }
+    const next = jest.fn()
+    rotas.notfound(req, res)
+    rotas.serverError( err, req, res, next)
+    expect(res.render.mock.lenght).toBe(undefined)
+    expect(res.render.mock.calls[0][0]).toBe('500')
 })
