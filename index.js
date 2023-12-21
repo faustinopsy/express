b764b00c95d82df362eaceb5232b8c363c320e55
faustinopsy <rodrigohipnose@gmail.com>
2023-12-21 16:31:11 -0300

    adicionando uma busca simples via API

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ app.get('/',rotas.home)
 
 app.get('/sobre',rotas.sobre)
 
+app.get('/contato',rotas.contato)
+
+app.get('/xxx', (req, res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers)
+    .map(([key, value]) => `${key} : ${value}`)
+    res.send(headers.join('\n'))
+})
+
+app.get('/api/produtos',rotas.getProdutos)
+
+app.get('/api/produtos/:id',rotas.getProdutosId)
+
 app.use(rotas.notfound)
 
 app.use(rotas.serverError)
 
-
+app.disable('x-powered-by')
 if(require.main === module){
     app.listen(port, () => {
     console.log(`Express inicado em http://localhost:${port}` + 
